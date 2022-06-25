a1aceeef218284fa2a0ae62d3b25759c13b3bdae
JasonFierro <78573851+JasonFierro@users.noreply.github.com>
2022-06-25 11:33:01 -0500

    Update TestBase.java
    prueba

--- TestBase.java
+++ TestBase.java
@@ -8,10 +8,6 @@ import pom.Movimiento.ReporteNominaPage;
 public class TestBase {
 
     protected WebDriver driver = Hooks.getDriver();
-    protected LoginPortalPage loginPortalPage = new LoginPortalPage(driver);
-    protected AdministrarNominaElectronicaPage administrarNominaElectronicaPage = new AdministrarNominaElectronicaPage(driver);
-    protected ReporteNominaPage reporteNominaPage = new ReporteNominaPage(driver);
-    protected colsanitasPage colsanitasPage = new colsanitasPage(driver);
     protected ToolsQAPage toolsQAPage = new ToolsQAPage(driver);
 
 }
